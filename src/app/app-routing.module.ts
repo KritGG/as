ba784ae92631ba75047c1cb11a7e13d91745f89a
Krittapat <krittapat.sy45@gmail.com>
2024-06-21 13:40:43 +0700

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', component: AppComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent},
   { path: '**', redirectTo: '' }
 
 ];
