3b4662e441f6227026517bdba30c0737e774b691
Krittapat <krittapat.sy45@gmail.com>
2024-06-21 13:44:33 +0700

    52

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent},
   { path: '**', redirectTo: '' }
